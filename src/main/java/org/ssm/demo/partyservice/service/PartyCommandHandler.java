2dc9a5c4e80603a05acc6736033d0908b18dc57a
Tola Awofolu <oawofolu@pivotal.io>
2020-08-30 06:03:29 -0400

    Testing

--- src/main/java/org/ssm/demo/partyservice/service/PartyCommandHandler.java
+++ src/main/java/org/ssm/demo/partyservice/service/PartyCommandHandler.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.stereotype.Service;
 import org.ssm.demo.partyservice.entity.Party;
 
@@ -13,9 +14,10 @@ public class PartyCommandHandler {
 Logger LOG = LoggerFactory.getLogger(PartyCommandHandler.class);
 	
 	@KafkaListener(topics = "dbserver1.party.parties", groupId = "party-consumer")
-	public Party suggested(Map<?,?> message) {
+	public Party suggested(Map<?,?> message, @Headers Map<?,?> headers) {
 		Party party = Party.of(message);
 		LOG.info(String.format("Message received: %s", party));
+		headers.forEach((key,value) -> LOG.info(String.format("Header %s: %s",key,value)));
 		return party;
 	}
 }
