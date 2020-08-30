de87fbe362cb0a3a9893fec493c6f24ac47334df
Tola Awofolu <oawofolu@pivotal.io>
2020-08-30 17:08:16 -0400

    Refactored code

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 docker-compose rm -svf
 docker-compose down
-docker-compose up -d --build & wait
+docker-compose up -d --build
 curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ --data-binary "@sql/postgres-connect-config.json"
