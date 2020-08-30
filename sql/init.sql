f8f995d229a7d48dd4c648f13f461c47107390ac
Tola Awofolu <oawofolu@pivotal.io>
2020-08-30 14:27:17 -0400

    Refactored code

--- sql/init.sql
+++ sql/init.sql
@@ -1,21 +1,24 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE SCHEMA party;
-CREATE TABLE party.parties (
+CREATE TABLE party.party (
   id uuid DEFAULT uuid_generate_v4 (),
   state varchar(100) ,
   rsvp_count int default 0,
   account_balance int ,
   max_attendees int default 0,
   created_at timestamp default current_timestamp,
-   CONSTRAINT parties_pkey PRIMARY KEY (id)
+   CONSTRAINT party_pkey PRIMARY KEY (id)
 )
 ;
-CREATE TABLE party.parties_outbox (
+CREATE TABLE party.party_outbox (
   id serial,
   event_id uuid,
   event_type varchar(100) ,
   payload varchar(5000) ,
   created_at timestamp default current_timestamp,
-  CONSTRAINT parties_outbox_pkey PRIMARY KEY (id)
+  CONSTRAINT party_outbox_pkey PRIMARY KEY (id)
 )
+;
+INSERT INTO party.party (state,rsvp_count,account_balance) VALUES 
+('proposed',NULL,NULL)
 ;
