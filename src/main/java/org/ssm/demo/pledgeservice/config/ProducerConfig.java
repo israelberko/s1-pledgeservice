39a291b9eb0f4011c8f5c8afa14e20a88d736a74
Tola Awofolu <oawofolu@pivotal.io>
2020-09-05 13:10:24 -0400

    Refactored code

--- src/main/java/org/ssm/demo/pledgeservice/config/ProducerConfig.java
+++ src/main/java/org/ssm/demo/pledgeservice/config/ProducerConfig.java
@@ -7,6 +7,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ProducerConfig {
 	@Bean public NewTopic donorInbox() {
-		return new NewTopic("donor.inbox", 2, (short)1);
+		return new NewTopic("donor.inbox", 1, (short)1);
 	}
 }
