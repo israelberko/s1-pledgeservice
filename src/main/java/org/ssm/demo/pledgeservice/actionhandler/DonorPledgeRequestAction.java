c2dd044e4f9d0b079518d0f1c1fbfd484c719c11
Tola Awofolu <oawofolu@pivotal.io>
2020-09-06 10:26:19 -0400

    Refactored code

--- src/main/java/org/ssm/demo/pledgeservice/actionhandler/DonorPledgeRequestAction.java
+++ src/main/java/org/ssm/demo/pledgeservice/actionhandler/DonorPledgeRequestAction.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.action.Action;
@@ -17,12 +18,13 @@ import org.ssm.demo.pledgeservice.statemachine.PledgeEvents;
 import org.ssm.demo.pledgeservice.statemachine.PledgeStates;
 
 @Service
-@EnableBinding(Sink.class)
+@EnableKafka
 public class DonorPledgeRequestAction implements Action<PledgeStates, PledgeEvents> {
 
 	Logger LOG = LoggerFactory.getLogger(DonorPledgeRequestAction.class);
 
 	@Override
+	@SendTo("donor.inbox")
 	public void execute(StateContext<PledgeStates, PledgeEvents> context) {
 		PledgeOutbox actionMessage =
 						PledgeOutbox.builder()
@@ -32,6 +34,7 @@ public class DonorPledgeRequestAction implements Action<PledgeStates, PledgeEven
 				
 
 		LOG.info("Sending from Action...{}", actionMessage);
+		LOG.info("About to send...{}", actionMessage);
 		this.sendToDestination(actionMessage);
 	}
 
