7413818469a3c8f0fac205bd65972542c79ede1a
Tola Awofolu <oawofolu@pivotal.io>
2020-09-06 10:30:39 -0400

    Refactor code

--- src/main/java/org/ssm/demo/pledgeservice/actionhandler/DonorPledgeRequestAction.java
+++ src/main/java/org/ssm/demo/pledgeservice/actionhandler/DonorPledgeRequestAction.java
@@ -4,21 +4,16 @@ import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.action.Action;
 import org.springframework.stereotype.Service;
 import org.ssm.demo.pledgeservice.entity.PledgeOutbox;
-import org.ssm.demo.pledgeservice.repositories.PledgeRepository;
 import org.ssm.demo.pledgeservice.statemachine.PledgeEvents;
 import org.ssm.demo.pledgeservice.statemachine.PledgeStates;
 
 @Service
-@EnableKafka
 public class DonorPledgeRequestAction implements Action<PledgeStates, PledgeEvents> {
 
 	Logger LOG = LoggerFactory.getLogger(DonorPledgeRequestAction.class);
@@ -37,6 +32,7 @@ public class DonorPledgeRequestAction implements Action<PledgeStates, PledgeEven
 		this.sendToDestination(actionMessage);
 	}
 
+	@KafkaListener(topics = "dbserver1.pledge.pledge_outbox", groupId = "pledge-consumer")
 	@SendTo("donor.inbox")
 	public PledgeOutbox sendToDestination(PledgeOutbox message) {
 		LOG.info("About to send...{}", message);
