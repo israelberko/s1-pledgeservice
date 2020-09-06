4224c08cfc3d7ee8e46587b14e195a6c750f2f4e
Tola Awofolu <oawofolu@pivotal.io>
2020-09-06 10:10:28 -0400

    Refactored code

--- src/main/java/org/ssm/demo/pledgeservice/actionhandler/DonorPledgeRequestAction.java
+++ src/main/java/org/ssm/demo/pledgeservice/actionhandler/DonorPledgeRequestAction.java
@@ -4,11 +4,13 @@ import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.action.Action;
 import org.springframework.stereotype.Service;
 import org.ssm.demo.pledgeservice.entity.PledgeOutbox;
+import org.ssm.demo.pledgeservice.repositories.PledgeRepository;
 import org.ssm.demo.pledgeservice.statemachine.PledgeEvents;
 import org.ssm.demo.pledgeservice.statemachine.PledgeStates;
 
@@ -16,6 +18,7 @@ import org.ssm.demo.pledgeservice.statemachine.PledgeStates;
 public class DonorPledgeRequestAction implements Action<PledgeStates, PledgeEvents> {
 
 	Logger LOG = LoggerFactory.getLogger(DonorPledgeRequestAction.class);
+	@Autowired PledgeRepository repo;
 
 	@Override
 	public void execute(StateContext<PledgeStates, PledgeEvents> context) {
@@ -27,6 +30,7 @@ public class DonorPledgeRequestAction implements Action<PledgeStates, PledgeEven
 				
 
 		LOG.info("Sending from Action...{}", actionMessage);
+		LOG.info("Repo count: {}", repo.count());
 		this.sendToDestination(actionMessage);
 	}
 
