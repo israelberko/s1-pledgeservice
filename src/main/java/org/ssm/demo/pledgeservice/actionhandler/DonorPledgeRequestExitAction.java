c30b200b43bb934aeb36f9aee255175fb33de6aa
Tola Awofolu <oawofolu@pivotal.io>
2020-09-07 05:01:58 -0400

    Refactored code

--- src/main/java/org/ssm/demo/pledgeservice/actionhandler/DonorPledgeRequestExitAction.java
+++ src/main/java/org/ssm/demo/pledgeservice/actionhandler/DonorPledgeRequestExitAction.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
 import org.ssm.demo.pledgeservice.entity.Pledge;
 import org.ssm.demo.pledgeservice.service.ContextService;
 import org.ssm.demo.pledgeservice.service.PledgeService;
@@ -14,6 +15,7 @@ import org.ssm.demo.pledgeservice.shared.Utils;
 import org.ssm.demo.pledgeservice.statemachine.PledgeEvents;
 import org.ssm.demo.pledgeservice.statemachine.PledgeStates;
 
+@Component
 public class DonorPledgeRequestExitAction implements Action<PledgeStates, PledgeEvents>{
 	
 	Logger LOG = LoggerFactory.getLogger(DonorPledgeRequestExitAction.class);
