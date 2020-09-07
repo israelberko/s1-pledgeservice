2d7fabde10884c33cc070c1642ba9ee4beaed1e5
Tola Awofolu <oawofolu@pivotal.io>
2020-09-06 20:34:13 -0400

    Refactored code

--- src/main/java/org/ssm/demo/pledgeservice/guardhandler/DonorPledgeRequestGuard.java
+++ src/main/java/org/ssm/demo/pledgeservice/guardhandler/DonorPledgeRequestGuard.java
@@ -1,5 +1,7 @@
 package org.ssm.demo.pledgeservice.guardhandler;
 
+import java.util.Map;
+
 import org.apache.commons.lang3.ObjectUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,12 +23,26 @@ public class DonorPledgeRequestGuard implements Guard<PledgeStates, PledgeEvents
 	@Override
 	public boolean evaluate(StateContext<PledgeStates, PledgeEvents> context) {
 		LOG.info("Invoking DonorPledgeRequestGuard");
+
+		Map<?,?> map = utils.getExtendedStateVar( context, "pledge", Map.class);
 		
-		Integer requestedAmount = ObjectUtils.defaultIfNull( 
-				utils.getExtendedStateVarAsInt(context, "requestedAmount"), 0);
+		Integer totalAmount = 
+				ObjectUtils.defaultIfNull(
+						utils.getAsInt(map, "actual_pledged_amount"), 0);
 		
-		Integer totalAmount     = ObjectUtils.defaultIfNull(
-				utils.getExtendedStateVarAsInt(context, "totalAmount"), 0);
+		totalAmount += 
+				ObjectUtils.defaultIfNull(
+						utils.getExtendedStateVarAsInt(context, "totalAmount"), 0);
+		
+		Integer requestedAmount = 
+				ObjectUtils.defaultIfNull( 
+						utils.getExtendedStateVarAsInt(context, "requestedAmount"), 0);
+
+		utils.setExtendedStateVar( context, "totalAmount", totalAmount );
+		
+		LOG.info("Comparing total amount - {} to requested amount - {} (total amount must not be less)", 
+				totalAmount, 
+					requestedAmount);
 		
 		return (totalAmount < requestedAmount);
 	}
