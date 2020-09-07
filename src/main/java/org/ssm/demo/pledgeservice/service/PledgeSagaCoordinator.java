72252028869ded3fcbe9723b264f73e90ad46270
Tola Awofolu <oawofolu@pivotal.io>
2020-09-07 12:59:14 -0400

    Refactored code

--- src/main/java/org/ssm/demo/pledgeservice/service/PledgeSagaCoordinator.java
+++ src/main/java/org/ssm/demo/pledgeservice/service/PledgeSagaCoordinator.java
@@ -19,7 +19,7 @@ public class PledgeSagaCoordinator {
 	
 	@SuppressWarnings("deprecation")
 	public void handleTrigger(PledgeEvents dispatchEvent, Map<String,?> extendedState, UUID pledge_id) {
-		LOG.info("Dispatching event {} to state machine from saga coordinator: {}", 
+		LOG.info("Dispatching event {} to state machine from saga coordinator: {}, {}", 
 				dispatchEvent, extendedState, stateMachine.getExtendedState().getVariables().get("pledge"));
 		
 		stateMachine.getExtendedState().getVariables().putAll(extendedState);
