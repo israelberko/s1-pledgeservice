209dc742f79f128201afab97c9dd4df4ec3f732d
Tola Awofolu <oawofolu@pivotal.io>
2020-09-07 06:33:26 -0400

    Refactored code

--- register_connectors.sh
+++ register_connectors.sh
@@ -1,3 +1,3 @@
 curl -X DELETE http://localhost:8083/connectors/
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ --data-binary "@sql/postgres-connect-config.json"
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ --data-binary "@../s1-donorservice/sql/postgres-connect-config.json"
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ --data-binary "@rest-requests/postgres-connect-config.json"
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ --data-binary "@../s1-donorservice/rest-requests/postgres-connect-config.json"
