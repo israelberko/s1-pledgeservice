ebf47416ca8ad3386c47d42000ea3900f5a3b24d
Tola Awofolu <oawofolu@pivotal.io>
2020-09-07 06:36:45 -0400

    Refactored code

--- register_connectors.sh
+++ register_connectors.sh
@@ -1,3 +1,4 @@
-curl -X DELETE http://localhost:8083/connectors/
+curl -X DELETE http://localhost:8083/connectors/pledge-outbox-connector
+curl -X DELETE http://localhost:8083/connectors/donation-outbox-connector
 curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ --data-binary "@rest-requests/postgres-connect-config.json"
 curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ --data-binary "@../s1-donorservice/rest-requests/postgres-connect-config.json"
