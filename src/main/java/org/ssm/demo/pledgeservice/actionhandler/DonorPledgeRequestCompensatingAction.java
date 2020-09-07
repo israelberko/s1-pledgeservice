04372b140085ea30f9f2e4821bd96d2c1f33c991
Tola Awofolu <oawofolu@pivotal.io>
2020-09-07 12:08:51 -0400

    Refactored code

---
+++ src/main/java/org/ssm/demo/pledgeservice/actionhandler/DonorPledgeRequestCompensatingAction.java
@@ -0,0 +1,59 @@
+package org.ssm.demo.pledgeservice.actionhandler;
+
+import java.util.Map;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Service;
+import org.ssm.demo.pledgeservice.entity.PledgeOutbox;
+import org.ssm.demo.pledgeservice.service.ContextService;
+import org.ssm.demo.pledgeservice.service.PledgeService;
+import org.ssm.demo.pledgeservice.shared.Utils;
+import org.ssm.demo.pledgeservice.statemachine.PledgeEvents;
+import org.ssm.demo.pledgeservice.statemachine.PledgeStates;
+
+@Service
+public class DonorPledgeRequestCompensatingAction implements Action<PledgeStates, PledgeEvents>{
+	
+	Logger LOG = LoggerFactory.getLogger(DonorPledgeRequestCompensatingAction.class);
+	
+	@Autowired ApplicationEventPublisher publisher;
+
+	@Autowired Utils utils;
+	
+	@Autowired ContextService contextService;
+	
+	@Autowired PledgeService pledgeService;
+
+	@Override
+	public void execute(StateContext<PledgeStates, PledgeEvents> context) {
+		
+		LOG.info("Invoking {}", this.getClass());
+	}
+
+	@KafkaListener(topics = "dbserver1.pledge.pledge_outbox", groupId = "pledge-cancel-consumer")
+	@SendTo("donor.cancel.inbox")
+	public PledgeOutbox sendToDonor(Map<?,?> message) {
+		PledgeOutbox outbox = PledgeOutbox.of(message);
+		
+		if (outbox.getEvent_type().equals(PledgeEvents.PLEDGE_CANCELLED.name())) {
+			
+			LOG.info("Sending cancellation to DonorService...{}", outbox);
+			
+			return outbox;
+			
+		} else {
+			
+			return null;
+			
+		}
+	}
+	
+}
