10569aed2d1a1102bfa05067f9f09805ae478449
Tola Awofolu <oawofolu@pivotal.io>
2020-09-07 10:24:23 -0400

    Refactored code

--- src/main/java/org/ssm/demo/pledgeservice/service/PledgeService.java
+++ src/main/java/org/ssm/demo/pledgeservice/service/PledgeService.java
@@ -1,21 +1,21 @@
 package org.ssm.demo.pledgeservice.service;
 
+import java.util.Map;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.event.EventListener;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.event.TransactionalEventListener;
 import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 import org.ssm.demo.pledgeservice.entity.Pledge;
 import org.ssm.demo.pledgeservice.entity.PledgeOutbox;
 import org.ssm.demo.pledgeservice.repositories.PledgeOutboxRepository;
 import org.ssm.demo.pledgeservice.repositories.PledgeRepository;
-import org.ssm.demo.pledgeservice.wrapperevent.CreatePledgeEvent;
 
 @Service
 public class PledgeService {
@@ -25,24 +25,24 @@ public class PledgeService {
 	@Autowired ApplicationEventPublisher applicationEventPublisher;
 	@Autowired PledgeRepository pledgeRepository;
 	
-//	@Transactional
-//	@KafkaListener(topics = "dbserver1.pledge.pledge", groupId = "pledge-consumer")
-//	public Pledge beforePledgeSave(Map<?,?> message) {
-//		Pledge pledge = Pledge.of(message);
-//		
-//		return createPledgeOutbox(pledge);
-//	}
-//	
-//	@Transactional
-//	public Pledge createPledgeOutbox(Pledge pledge) {
-//		PledgeOutbox pledgeOutbox = PledgeOutbox.from(pledge);
-//		
-//		LOG.info("Before saving: Pledge: {}\nAnd PledgeOutbox: {}", pledge, pledgeOutbox);
-//		
-//		applicationEventPublisher.publishEvent(pledgeOutbox);
-//		
-//		return pledge;
-//	}
+	@Transactional
+	@KafkaListener(topics = "dbserver1.pledge.pledge", groupId = "pledge-consumer")
+	public Pledge beforePledgeSave(Map<?,?> message) {
+		Pledge pledge = Pledge.of(message);
+		
+		return createPledgeOutbox(pledge);
+	}
+	
+	@Transactional
+	public Pledge createPledgeOutbox(Pledge pledge) {
+		PledgeOutbox pledgeOutbox = PledgeOutbox.from(pledge);
+		
+		LOG.info("Before saving: Pledge: {}\nAnd PledgeOutbox: {}", pledge, pledgeOutbox);
+		
+		applicationEventPublisher.publishEvent(pledgeOutbox);
+		
+		return pledge;
+	}
 //	@Bean
 //	public Consumer<Map<?,?>> createPledgeOutbox() {
 //		return message -> {
@@ -54,14 +54,13 @@ public class PledgeService {
 //	}
 	
 
-	@TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT, classes = CreatePledgeEvent.class) 
-	@EventListener(classes = CreatePledgeEvent.class)
-	public void acceptOutboxEvent(CreatePledgeEvent<PledgeOutbox> event){
-		LOG.info("Captured! Outbox: {}", event);
+	@TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+	public void acceptOutboxEvent(PledgeOutbox event){
+		LOG.info("Outbox: {}", event);
 		
-		pledgeOutboxRepository.save(event.getOutbox());
+		pledgeOutboxRepository.save(event);
 		
-		pledgeOutboxRepository.delete(event.getOutbox());
+		pledgeOutboxRepository.delete(event);
 	}
 	
 //	@Transactional
