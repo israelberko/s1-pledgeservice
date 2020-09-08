714c4c86f491f6f3b14bdf2f6e7ffd941f413452
Tola Awofolu <oawofolu@pivotal.io>
2020-09-08 18:44:47 -0400

    Debugging

--- src/main/java/org/ssm/demo/pledgeservice/statemachine/service/PledgeStateMachineService.java
+++ src/main/java/org/ssm/demo/pledgeservice/statemachine/service/PledgeStateMachineService.java
@@ -27,14 +27,19 @@ public class PledgeStateMachineService {
 	@EventListener(classes = LoadStateMachineEvent.class)
 	public StateMachine<PledgeStates, PledgeEvents> getStateMachine(UUID pledge_id) {
 		
+		LOG.info("was here?");
+		try {
 		StateMachine<PledgeStates, PledgeEvents> stateMachine =
 				stateMachineStore.getOrDefault(
 						pledge_id,
 							stateMachineFactory.getStateMachine(pledge_id));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		LOG.info("no worries");
 		
 		stateMachineStore.put( pledge_id, stateMachine );
-		
-		LOG.info("STate madhine is {}", stateMachine);
 		
 		return stateMachine;
 	}
