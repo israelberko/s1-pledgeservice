bd88cebe6b42e178c144d1bda68311eac0ad2d69
Tola Awofolu <oawofolu@pivotal.io>
2020-09-09 08:30:45 -0400

    Troubleshooting

--- src/main/java/org/ssm/demo/pledgeservice/repositoryevents/PledgeRepositoryEventHandler.java
+++ src/main/java/org/ssm/demo/pledgeservice/repositoryevents/PledgeRepositoryEventHandler.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
 import org.ssm.demo.pledgeservice.applicationevents.LoadStateMachineEvent;
 import org.ssm.demo.pledgeservice.entity.Pledge;
 
 @RepositoryEventHandler(Pledge.class) 
+@Component
 public class PledgeRepositoryEventHandler {
 	@Autowired ApplicationEventPublisher applicationEventPublisher;
 	Logger LOG = LoggerFactory.getLogger("Class AuthorEventHandler");
