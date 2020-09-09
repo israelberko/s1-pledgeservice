ddaa5720d1cf25d7105bcd0a0c3b28aeb3c290e4
Tola Awofolu <oawofolu@pivotal.io>
2020-09-09 01:54:07 -0400

    Troubleshooting

--- src/main/java/org/ssm/demo/pledgeservice/statemachine/service/PledgeStateMachineService.java
+++ src/main/java/org/ssm/demo/pledgeservice/statemachine/service/PledgeStateMachineService.java
@@ -9,8 +9,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.statemachine.StateMachine;
-import org.springframework.statemachine.config.StateMachineF
-import org.ssm.demo.pledgeservice.applicationevents.LoadStateMachineEvent;actory;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.ssm.demo.pledgeservice.applicationevents.LoadStateMachineEvent;
 import org.springframework.stereotype.Component;
 import org.ssm.demo.pledgeservice.shared.PledgeEvents;
 import org.ssm.demo.pledgeservice.shared.PledgeStates;
