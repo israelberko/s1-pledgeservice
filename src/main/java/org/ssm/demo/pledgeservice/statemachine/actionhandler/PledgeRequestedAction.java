356ea2bdde996d732dabd1b45b4f0727209f1ae1
Tola Awofolu <oawofolu@pivotal.io>
2020-09-09 01:46:14 -0400

    Troubleshooting

--- src/main/java/org/ssm/demo/pledgeservice/statemachine/actionhandler/PledgeRequestedAction.java
+++ src/main/java/org/ssm/demo/pledgeservice/statemachine/actionhandler/PledgeRequestedAction.java
@@ -67,7 +67,7 @@ public class PledgeRequestedAction implements Action<PledgeStates, PledgeEvents>
 	
 		Pledge pledge = Pledge.of(utils.getExtendedStateVar(context, "pledge", Map.class));
 		
-		pledge.setUpdated_at(new Timestamp(System.currentTimeMillis()));
+//		pledge.setUpdated_at(new Timestamp(System.currentTimeMillis()));
 		
 		pledgeService.savePledge( pledge );
 	}
