f012ec5f5359e16c920abbfea0aff6f95d9b6957
Tola Awofolu <oawofolu@pivotal.io>
2020-09-11 11:38:30 -0400

    Troubleshooting

--- src/main/java/org/ssm/demo/pledgeservice/statemachinehandler/PledgeStateMachineCommandHandler.java
+++ src/main/java/org/ssm/demo/pledgeservice/statemachinehandler/PledgeStateMachineCommandHandler.java
@@ -60,9 +60,16 @@ public class PledgeStateMachineCommandHandler {
 	}
 	
 	@EventListener(condition = "#pledgeOutbox.event_type eq 'PLEDGE_CANCEL_REQUESTED'") 
-	public void handleDonorCancelRequest(PledgeOutbox pledgeOutbox) {
+	public void handleInitializeDonorCancelRequest(PledgeOutbox pledgeOutbox) {
 		
 		coordinator.handleTrigger(PledgeEvents.PLEDGE_CANCEL_REQUESTED, ImmutableMap.of("donor",pledgeOutbox.getPayloadAsMap()), pledgeOutbox.getEvent_id());
+	
+	}
+	
+	@EventListener(condition = "#pledgeOutbox.event_type eq 'PLEDGE_CANCEL_REQUESTED_PENDING'")
+	public void handleDonorCancelRequest(PledgeOutbox pledgeOutbox) {
+		
+		coordinator.handleTrigger(PledgeEvents.PLEDGE_CANCEL_REQUESTED, ImmutableMap.of("pledge",pledgeOutbox.getPayloadAsMap()), pledgeOutbox.getEvent_id());
 	
 	}
 	
