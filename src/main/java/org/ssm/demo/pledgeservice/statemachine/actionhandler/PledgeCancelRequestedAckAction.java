1798cb635aa3c40fe7a70d873862fd15deaa0c77
Tola Awofolu <oawofolu@pivotal.io>
2020-09-11 14:24:25 -0400

    Troubleshooting

--- src/main/java/org/ssm/demo/pledgeservice/statemachine/actionhandler/PledgeCancelRequestedAckAction.java
+++ src/main/java/org/ssm/demo/pledgeservice/statemachine/actionhandler/PledgeCancelRequestedAckAction.java
@@ -7,20 +7,15 @@ import org.springframework.statemachine.action.Action;
 import org.springframework.stereotype.Component;
 import org.ssm.demo.pledgeservice.shared.PledgeEvents;
 import org.ssm.demo.pledgeservice.shared.PledgeStates;
-import org.ssm.demo.pledgeservice.shared.Utils;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Component
 public class PledgeCancelRequestedAckAction implements Action<PledgeStates, PledgeEvents>{
 	Logger LOG = LoggerFactory.getLogger(PledgeCancelRequestedAckAction.class);
-	@Autowired Utils utils;
 
 	@Override
 	public void execute(StateContext<PledgeStates, PledgeEvents> context) {
 		LOG.info("Invoking {}", this.getClass());
-		
-		utils.setExtendedStateVar(context, "cancelRequestSuccess", Boolean.TRUE);
 		
 		LOG.info("\n\n===========================\nCancel SUCCESSFUL: TRUE\n===========================\n\n");
 
--- src/main/java/org/ssm/demo/pledgeservice/statemachine/actionhandler/PledgeCancelRequestedNackAction.java
+++ src/main/java/org/ssm/demo/pledgeservice/statemachine/actionhandler/PledgeCancelRequestedNackAction.java
@@ -7,20 +7,15 @@ import org.springframework.statemachine.action.Action;
 import org.springframework.stereotype.Component;
 import org.ssm.demo.pledgeservice.shared.PledgeEvents;
 import org.ssm.demo.pledgeservice.shared.PledgeStates;
-import org.ssm.demo.pledgeservice.shared.Utils;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Component
 public class PledgeCancelRequestedNackAction implements Action<PledgeStates, PledgeEvents>{
 	Logger LOG = LoggerFactory.getLogger(PledgeCancelRequestedNackAction.class);
-	@Autowired Utils utils;
 
 	@Override
 	public void execute(StateContext<PledgeStates, PledgeEvents> context) {
 		LOG.info("Invoking {}", this.getClass());
-		
-		utils.setExtendedStateVar(context, "cancelRequestSuccess", Boolean.FALSE);
 		
 		LOG.info("\n\n===========================\nCancel SUCCESSFUL: FALSE\n===========================\n");
 
