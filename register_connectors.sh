e72b7775fd39d65464b7afe659c7f03f7361802e
israelb <israelb>
2021-12-06 15:42:09 +0200

    Disable the registration for Debezium

--- register_connectors.sh
+++ register_connectors.sh
@@ -1,6 +1,6 @@
 #curl -X DELETE  http://localhost:8083/connectors/pledge-connector
 curl -X DELETE  http://localhost:8083/connectors/pledge-outbox-connector
-curl -X DELETE http://localhost:8083/connectors/donation-outbox-connector
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ --data-binary "@rest-requests/postgres-connect-config-pledge.json"
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ --data-binary "@rest-requests/postgres-connect-config-pledge-outbox.json"
+#curl -X DELETE http://localhost:8083/connectors/donation-outbox-connector
+#curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ --data-binary "@rest-requests/postgres-connect-config-pledge.json"
+#curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ --data-binary "@rest-requests/postgres-connect-config-pledge-outbox.json"
 #curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ --data-binary "@../s1-donorservice/rest-requests/postgres-connect-config-pledge.json"
